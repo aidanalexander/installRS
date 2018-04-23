597000e90c6b5a6919485d9adbc7d14d971f6dac
Your Name <aidanalexander100@gmail.com>
2018-04-23 22:08:53 +0100

    feat: included PyAutoGui & pip3 installation

--- installOSRS.sh
+++ installOSRS.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apt update
-apt install wget
-apt install 
-cd
-mkdir -p OSRS
-cd OSRS
-mkdir -p osbuddyJar
-wget
+apt install update
+apt install default-jre
+apt install python3
+apt install python3-pip
+apt install python3-xlib
+
+pip3 show PyAutoGUI
+
